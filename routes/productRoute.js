0e06f20da9a7e8b1d6277d789b0bf7e698c84c79
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-07-30 13:24:20 +0530

    removed getproduct()

--- routes/productRoute.js
+++ routes/productRoute.js
@@ -1,7 +1,5 @@
-import express from "express"
-import {getAllProducts}  from "../controllers/productController.js"
-import {getProduct}  from "../controllers/productController.js"
+import express from "express";
+import { getAllProducts } from "../controllers/productController.js";
 const router = express.Router();
 router.get("/products", getAllProducts);
-router.get("/products/:id", getProduct);
 export default router;

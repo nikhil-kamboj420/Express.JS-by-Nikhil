a02c81f6314e462b4a6a6cac78621794374a2b94
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-02 14:40:43 +0530

    add postLogin

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,4 +1,8 @@
 import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+
 export const getRegisterPage = (req, res) => {
     const file = path.join(`${import.meta.dirname}`, '../views/register.html');
     res.sendFile(file, (err) => {
@@ -9,10 +13,17 @@ export const getRegisterPage = (req, res) => {
 };
 
 export const getLoginPage = (req, res) => {
-    const file = path.join(`${import.meta.dirname}`, '../views/login.html');
-    res.sendFile(file, (err) => {
-        if (err) {
-            res.status(500).send("Error loading login page: " + err.message);
-        }
-    });
+  const file = path.join(__dirname, "../views/login.html");
+  res.sendFile(file, (err) => {
+    if (err) {
+      res.status(500).send("Error loading login page: " + err.message);
+    }
+  });
+};
+
+
+export const postLogin = (req, res) => {
+  res.setHeader("Set-Cookie", "isLoggedIn=true; path=/");
+  const file = path.join(__dirname, "../views/login-success.html");
+  res.sendFile(file);
 };

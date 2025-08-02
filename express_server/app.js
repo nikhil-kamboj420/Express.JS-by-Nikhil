14e2db7b302a2c608b7bfaadd13b40fad1b19910
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-02 16:35:38 +0530

    using cookie parser

--- express_server/app.js
+++ express_server/app.js
@@ -1,12 +1,15 @@
 import express from "express";
 import cors from "cors";
+import path from "path";
 // Import routes
 import dyRouteParam from "../routes/dyRouteParam.js";
 import queryParamRouter from "../routes/queryParam.js";
 import formRoutes from "../formsubmission/getForm.js";
 import handle404 from "../routes/handle404.js";
 import productRoute from "../routes/productRoute.js";
-
+import connectDB from "../mongodb/db_conn.js";
+import authRoute from "../routes/authRoute.js";
+import cookieParser from "cookie-parser";
 const app = express();
 
 // allow CORS for specific origin
@@ -17,12 +20,15 @@ app.use(
 );
 // Middleware to parse form data
 app.use(express.urlencoded({ extended: true }));
+// Serve static files (for CSS/JS if needed)
+app.use(express.static(path.join(process.cwd(), "views")));
+app.use(cookieParser())
 // Use routes
 app.use("/", dyRouteParam);
 app.use("/", queryParamRouter);
 app.use("/", formRoutes);
 app.use("/", productRoute);
-
+app.use("/", authRoute);
 // 404 handler middleware
 app.use(handle404);
 
@@ -31,3 +37,5 @@ const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+connectDB();

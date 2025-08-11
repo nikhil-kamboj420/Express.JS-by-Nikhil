639502b2f301922ecd02d50c59e12b052df1c103
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-11 13:26:16 +0530

    add loginUser validation and password hashing

--- mysql/storeData.js
+++ mysql/storeData.js
@@ -1,5 +1,5 @@
 import connectSqlDB from "./db_conn.js";
-
+import bcrypt from "bcryptjs";
 export const UserData = async (username, password, email) => {
   try {
     console.log("Creating user with data:", { username, password, email });
@@ -15,10 +15,13 @@ export const UserData = async (username, password, email) => {
     await db.query(usertable);
     console.log("✅ usertable created (or already exists)");
 
+    //? 🔐 password hashing
+const hashedPassword  = await bcrypt.hash(password,10)
+
     // !insert  user into usertable
     const insertQuery =
       "INSERT INTO usertable (username, password, email) VALUES (?, ?, ?)";
-    await db.query(insertQuery, [username, password, email]);
+    await db.query(insertQuery, [username, hashedPassword, email]);
     console.log("User inserted");
 
     // ! fetching all users from usertable
@@ -26,5 +29,31 @@ export const UserData = async (username, password, email) => {
     console.log(rows);
   } catch (err) {
     console.error("❌ Error creating usertable:", err.message);
+  }
+};
+
+export const loginUser = async (email, password,res) => {
+  try {
+    const db = await connectSqlDB();
+
+    // 1) Check user exist
+    const [rows] = await db.query("SELECT * FROM usertable WHERE email = ?", [email]);
+    if (rows.length === 0) {
+      console.log("❌ User not found");
+      return;
+    }
+
+    const user = rows[0];
+
+    // 2) Compare password with hash
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) {
+      console.log("❌ Invalid password");
+      return res.status(401).send({ message: "Invalid email or password" });
+    }
+    res.status(201).send("User found successfully");
+    console.log("✅ Login successful", user);
+  } catch (err) {
+    console.error("❌ Error logging in:", err.message);
   }
 };

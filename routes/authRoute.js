f38add58cdac51d46baf2bead097d1ea111b00e3
nikhil-kamboj420 <kambojnikhil44@gmail.com>
2025-08-12 11:55:28 +0530

    add verifyToken and logout route

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,13 +1,19 @@
 import express from "express";
 import * as authControllers from "../controllers/authController.js";
+import { verifyToken } from "../middleware/verifyToken.js";
+
 const router = express.Router();
+
 router.route("/register")
   .get(authControllers.getRegisterPage)
   .post(authControllers.postRegister);
-router
-  .route("/login")
+
+router.route("/login")
   .get(authControllers.getLoginPage)
   .post(authControllers.postLogin);
 
-  router.get('/admin.html',authControllers.showProtectedRoute);
+router.get("/admin.html", verifyToken, authControllers.showProtectedRoute);
+
+router.post("/logout", authControllers.logoutUser);
+
 export default router;
